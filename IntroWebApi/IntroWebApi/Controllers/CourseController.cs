43908b99aa52d3ae30e8984e4ef89b6d6857e96e
KaziTanzil <kazitanjilkazitanjil@gmail.com>
2025-09-13 10:25:54 -0600

    Intro of Web Api

---
+++ IntroWebApi/IntroWebApi/Controllers/CourseController.cs
@@ -0,0 +1,86 @@
+﻿using IntroWebApi.Models;
+using IntroWebApi.Models.vM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroWebApi.Controllers
+{
+    [RoutePrefix("api/Course")]
+    public class CourseController : ApiController
+    {
+        //by default api maps method with request verbs
+        public List<CourseModel> Get()
+        {
+            List<CourseModel> course=new List<CourseModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                course.Add(new CourseModel { Id = i + 1, Name = "Course" + i });
+            }
+            return course;
+            //RegistrationEntities db = new RegistrationEntities();
+            //return db.Cources.ToList();
+        }
+
+
+        public IHttpActionResult Get(int id)
+        {
+           
+            return Ok("Hello from Tanzil "+id);
+        }
+
+
+        public Message Post()
+        {
+            Message m = new Message();
+ 
+            m.Msg = "Hello";
+            return m;
+        }
+
+
+        [HttpPost]
+        [Route("PostData")]
+        public string PostData(CourseModel c)
+        {
+            List<CourseModel> course = new List<CourseModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                course.Add(new CourseModel { Id = i + 1, Name = "Course" + i });
+            }
+            course.Add(c);
+            return "Course Added Successfully. "+c.Id+" "+c.Name;
+        }
+
+        [HttpPost]
+        [Route("PostHi")]
+        public IHttpActionResult PostHi()
+        {
+            var m = "Fuck you";
+            return Ok(m);
+        }
+        [HttpPost]
+        [Route("PostBye")]
+        public HttpResponseMessage PostBye()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Fuck you too");
+        }
+
+        [HttpPut]
+        public HttpResponseMessage Put()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Put my Dick into your hole");
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "Delete";
+        }
+
+
+    }
+}
---
+++ IntroWebApi/IntroWebApi/Controllers/CourseDbController.cs
@@ -0,0 +1,45 @@
+﻿using IntroWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroWebApi.Controllers
+{
+    [RoutePrefix("api/CourseDb")]
+    public class CourseDbController : ApiController
+    {
+        [HttpGet]
+        public List<Cource> Get()
+        {
+
+            RegistrationEntities db = new RegistrationEntities();
+            var data = new List<Cource>();
+            foreach(var c in db.Cources)
+            {
+                var course = new Cource()
+                {
+                    CId = c.CId,
+                    CName=c.CName,
+                    TCapacity=c.TCapacity,
+                    Count=c.Count
+                };
+
+                data.Add(course);
+                
+            }
+            return data;
+        }
+
+        [HttpGet]
+        [Route("GetName")]
+        public List<String> GetName()
+        {
+            RegistrationEntities db = new RegistrationEntities();
+            var name = (from c in db.Cources select c.CName).ToList();
+            return name;
+        }
+    }
+}
